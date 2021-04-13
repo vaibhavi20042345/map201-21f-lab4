2cc7a598d5e49a3563844c7ef86668805b7ebc2e
vaibhavi20042345 <20042345@saultcollege.ca>
2021-04-13 14:35:14 -0400

    Lab 5 part b point 20

---
+++ Photorama/Tag+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Tag+CoreDataClass.swift
+//  Photorama
+//
+//  Created by vaibhavi on 2021-04-13.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Tag)
+public class Tag: NSManagedObject {
+
+}
---
+++ Photorama/Tag+CoreDataProperties.swift
@@ -0,0 +1,43 @@
+//
+//  Tag+CoreDataProperties.swift
+//  Photorama
+//
+//  Created by vaibhavi on 2021-04-13.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Tag {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Tag> {
+        return NSFetchRequest<Tag>(entityName: "Tag")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var photos: NSSet?
+
+}
+
+// MARK: Generated accessors for photos
+extension Tag {
+
+    @objc(addPhotosObject:)
+    @NSManaged public func addToPhotos(_ value: Photo)
+
+    @objc(removePhotosObject:)
+    @NSManaged public func removeFromPhotos(_ value: Photo)
+
+    @objc(addPhotos:)
+    @NSManaged public func addToPhotos(_ values: NSSet)
+
+    @objc(removePhotos:)
+    @NSManaged public func removeFromPhotos(_ values: NSSet)
+
+}
+
+extension Tag : Identifiable {
+
+}
